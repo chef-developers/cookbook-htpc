4b5627fe3d091a34a80a796978322cd953d38710
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2012-07-19 14:03:16 +0200

    init

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Lukasz Kaniowski"
+maintainer_email "lukasz.kaniowski@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures programs used by htpc"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+depends 'apt'
